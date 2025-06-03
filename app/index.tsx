91341bac378b2e3a3acbbaa5935f7aed0c11e8e9
Selasie5 <selasisepenu5@gmail.com>
2025-06-03 13:32:40 +0000

    feat:exploring styling, text,view and image

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View ,Image} from 'react-native'
 import React from 'react'
+import Logo from "../assets/Logo.png"
 
 const index = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+    
+<Image source={Logo}/>
+
+      
+      <Text style={styles.title}>Reading List 1</Text>
+      <Text style={{marginTop:10, marginBottom:30}}>Test</Text>
+      <View style={styles.card}>
+        <Text>Hello, this is a card</Text>
+      </View>
     </View>
   )
 }
 
 export default index
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+  container:{
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title:{
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  card:{
+    backgroundColor: "#eee",
+    padding:20,
+    borderRadius:10,
+    boxShadow:'0 2px 4px rgba(0,0,0,0.1)',
+  },
+ 
+  img:{
+    resizeMode:'cover'
+  }
+})
